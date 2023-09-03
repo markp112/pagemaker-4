f56246f51f5c69fb5771f64e731e33225f8fcb75
mark phillips <mark.phillips1965@googlemail.com>
2023-09-03 13:12:58 +0200

    cleaned up dead files

--- back/src/core/services/firebase/common/index.ts
+++
@@ -1,8 +0,0 @@
-import { FIREBASE_URLS } from '../urls/urls';
-
-function getSiteAndVersionUrl(siteId: string, versionId: string): string {
-  const populateSiteStub = `${FIREBASE_URLS.populateSiteFiles.replace('<SITE_ID>', siteId).replace('<VERSION_ID>', versionId)}`;
-  return `${FIREBASE_URLS.firebaseBaseUrl}${populateSiteStub}`;
-}
-
-export { getSiteAndVersionUrl };
