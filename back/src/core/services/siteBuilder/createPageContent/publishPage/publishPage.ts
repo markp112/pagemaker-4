cf7a08bf01d4cc675c2440f54ae84446c7e33171
mark phillips <mark.phillips1965@googlemail.com>
2023-09-03 12:57:14 +0200

    updated publish page not saved

--- back/src/core/services/siteBuilder/createPageContent/publishPage/publishPage.ts
+++ back/src/core/services/siteBuilder/createPageContent/publishPage/publishPage.ts
@@ -8,11 +8,7 @@ function pagePublisher() {
 
   async function writeLocalFile(folder: FolderAndPage): Promise<void> {
     try {
-      const filePath = fileService.resolvePath(folder.pathToFile);
-      if (! await fileService.isFolderExisting(filePath)) {
-        await fileService.mkdir(filePath);
-      }
-      const fileToWrite = fileService.joinPath(filePath, `${folder.pageName}.${folder.type}`);
+      const fileToWrite = fileService.joinPath(folder.resolvedPathToFile, `${folder.filename}.${folder.type}`);
       await fileService.writeFile(fileToWrite, folder.fileContent);
     } catch (err) {
       handleError(err);
