53c078409bf220dcd5336080cde406fab57a3cdc
mark phillips <mark.phillips1965@googlemail.com>
2023-09-23 17:14:46 +0200

    fix code check issues

--- front/pagemaker/src/components/page/renderHtml/renderHtml.vue
+++ front/pagemaker/src/components/page/renderHtml/renderHtml.vue
@@ -5,14 +5,11 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
 import { useNavMenuItemStore } from '@/stores/navMenuItems.store';
-import { useRouter } from 'vue-router';
 import { usePageStore } from '@/stores/page.store';
 
 const HIDE_MENU_BAR = false;
 const pageContent = ref('');
 const navStore = useNavMenuItemStore();
-
-console.log(useRouter())
 
 onMounted(() => {
   pageContent.value = usePageStore().pageAsHtml;
